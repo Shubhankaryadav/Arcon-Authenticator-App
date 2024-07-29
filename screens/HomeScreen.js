df20d254233e5bd4e636c87e87af209e5c83e7c1
Shubhankar Yadav <shubhankaryadav08@gmail.com>
2024-07-29 13:50:35 +0530

    Basic Style changes (Home Screen)

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,17 +1,17 @@
-// screens/HomeScreen.js
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>Welcome to ARCON!</Text>
+      <Text style={styles.welcomeText}>Welcome to ARCON!</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  welcomeText: { fontSize: 24, frontWeight: 'bold'}
 });
 
 export default HomeScreen;
